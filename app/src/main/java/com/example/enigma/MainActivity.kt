c1b56d3d345e920e150dcab9b3217f38bcd23ce2
Romulus Ruja <romulus-emanuel.ruja@tutanota.com>
2023-08-06 14:49:34 +0300

    implement navigation

--- app/src/main/java/com/example/enigma/MainActivity.kt
+++ app/src/main/java/com/example/enigma/MainActivity.kt
@@ -2,9 +2,8 @@ package com.example.enigma
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.example.enigma.crypto.CryptoProvider
-import com.example.enigma.crypto.DataProvider
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,5 +16,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadFragment(ChatsFragment())
+        val bottomNav : BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.chatsFragment -> {
+                    loadFragment(ChatsFragment())
+                    true
+                }
+                R.id.addContactsFragment -> {
+                    loadFragment(AddContactsFragment())
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.navHostFragment,fragment)
+        transaction.commit()
     }
 }
