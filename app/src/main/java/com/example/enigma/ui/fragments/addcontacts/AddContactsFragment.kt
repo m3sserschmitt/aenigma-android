9b22671d51446d7c7fa34840951b3f174629e7a9
Romulus Ruja <romulus-emanuel.ruja@tutanota.com>
2023-09-21 23:37:01 +0300

    add qr code display functionality

--- app/src/main/java/com/example/enigma/ui/fragments/addcontacts/AddContactsFragment.kt
+++ app/src/main/java/com/example/enigma/ui/fragments/addcontacts/AddContactsFragment.kt
@@ -1,18 +1,63 @@
 package com.example.enigma.ui.fragments.addcontacts
 
+import android.content.res.Configuration
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.enigma.R
+import androidx.fragment.app.Fragment
+import com.example.enigma.databinding.FragmentAddContactsBinding
+import com.example.enigma.util.Constants.Companion.HEX_ADDRESS
+import com.example.enigma.util.Constants.Companion.PUBLIC_KEY
+import com.example.enigma.util.QrCodeGenerator
+import org.json.JSONObject
 
 class AddContactsFragment : Fragment() {
+
+    private lateinit var binding: FragmentAddContactsBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_contacts, container, false)
+    ): View {
+        binding = FragmentAddContactsBinding.inflate(inflater, container, false)
+
+        showCode()
+
+        return binding.root
+    }
+
+    private fun showCode()
+    {
+        val exportedData = ExportedData(HEX_ADDRESS, PUBLIC_KEY)
+        var size = 0
+        size = if (resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            binding.qrCodeImageView.height
+        } else {
+            binding.qrCodeImageView.width
+        }
+
+        val bitmap = QrCodeGenerator(size, size).encodeAsBitmap(exportedData.toString())
+
+        binding.qrCodeImageView.setImageBitmap(bitmap)
+    }
+
+    class ExportedData constructor(
+        private val address: String,
+        private val publicKey: String) {
+
+        override fun toString(): String {
+            val data = JSONObject()
+
+            try {
+                data.put("address", address)
+                data.put("publicKey", publicKey)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                return ""
+            }
+
+            return data.toString()
+        }
     }
 }
